faaae5dcd4f6d4a209270fb5a156b29b25ded682
geral-256 <geral141100@gmail.com>
2022-11-04 20:05:57 +0700

    menambahkan page cubit pada home

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,6 +1,11 @@
+import 'package:airplane_apps/cubit/page_cubit.dart';
 import 'package:airplane_apps/ui/pages/home_page.dart';
+import 'package:airplane_apps/ui/pages/settings_page.dart';
+import 'package:airplane_apps/ui/pages/transaction_page.dart';
+import 'package:airplane_apps/ui/pages/wallet_page.dart';
 import 'package:airplane_apps/ui/widgets/custom_bottom_navigation_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../shared/theme.dart';
 
 class MainPage extends StatelessWidget {
@@ -8,8 +13,19 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget buildContent() {
-      return HomePage();
+    Widget buildContent(int currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+        case 1:
+          return TransactionPage();
+        case 2:
+          return WalletPage();
+        case 3:
+          return SettingsPage();
+        default:
+          return HomePage();
+      }
     }
 
     Widget customBottomNavigation() {
@@ -32,16 +48,19 @@ class MainPage extends StatelessWidget {
             children: [
               CustomBottomNavigationItem(
                 imageUrl: 'assets/images/icon_home.png',
-                isSelected: true,
+                index: 0,
               ),
               CustomBottomNavigationItem(
                 imageUrl: 'assets/images/icon_booking.png',
+                index: 1,
               ),
               CustomBottomNavigationItem(
                 imageUrl: 'assets/images/icon_card.png',
+                index: 2,
               ),
               CustomBottomNavigationItem(
                 imageUrl: 'assets/images/icon_settings.png',
+                index: 3,
               ),
             ],
           ),
@@ -49,14 +68,18 @@ class MainPage extends StatelessWidget {
       );
     }
 
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: Stack(
-        children: [
-          buildContent(),
-          customBottomNavigation(),
-        ],
-      ),
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          backgroundColor: kBackgroundColor,
+          body: Stack(
+            children: [
+              buildContent(currentIndex),
+              customBottomNavigation(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
